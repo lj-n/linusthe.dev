bd5b245a7d209fb6d5d7e2f35924d17b1b265ff6
lj-n <linus.johannsen@outlook.com>
2023-01-29 23:20:11 +0100

    Add Vercel Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
   integrations: [
     tailwind({
       config: {
@@ -19,4 +20,5 @@ export default defineConfig({
     }),
     svelte(),
   ],
+  adapter: vercel(),
 });
