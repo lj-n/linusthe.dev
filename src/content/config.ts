86aaced8eca741bb316f62d39eeafb63c10ad9c6
lj-n <linus.johannsen@outlook.com>
2023-02-09 23:04:32 +0100

    add better tags, combine posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,8 +3,10 @@ import { defineCollection, z } from "astro:content";
 /** Blog Collection */
 const TagSchema = z.union([
   z.literal("JavaScript"),
-  z.literal("Animations"),
+  z.literal("Animation"),
   z.literal("CSS"),
+  z.literal("Goody"),
+  z.literal("Tutorial"),
 ]);
 export type Tag = z.infer<typeof TagSchema>;
 
@@ -23,7 +25,9 @@ const thingsCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
+    draft: z.boolean().optional(),
     date: z.string().optional(),
+    tags: z.array(TagSchema),
   }),
 });
 
