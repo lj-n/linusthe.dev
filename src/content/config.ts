def4a66a2473ab53ba3e05084d8e2fd6c1bf2d8a
lj-n <linus.johannsen@outlook.com>
2023-03-04 16:15:25 +0100

    combine post with startpage

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 import { defineCollection, z } from "astro:content";
 
-/** Blog Collection */
+/** Tags */
 const TagSchema = z.union([
   z.literal("JavaScript"),
   z.literal("Animation"),
@@ -8,31 +8,18 @@ const TagSchema = z.union([
   z.literal("Goody"),
   z.literal("Tutorial"),
 ]);
+
 export type Tag = z.infer<typeof TagSchema>;
 
-const blogCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    draft: z.boolean().optional(),
-    date: z.string().optional(),
-    tags: z.array(TagSchema),
-  }),
-});
-
 /** Things Collection */
-const thingsCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    draft: z.boolean().optional(),
-    date: z.string().optional(),
-    tags: z.array(TagSchema),
-  }),
-});
-
-/** Export all collections */
 export const collections = {
-  blog: blogCollection,
-  things: thingsCollection,
+  things: defineCollection({
+    schema: z.object({
+      title: z.string(),
+      description: z.string(),
+      draft: z.boolean().optional(),
+      date: z.string().optional(),
+      tags: z.array(TagSchema),
+    }),
+  }),
 };
